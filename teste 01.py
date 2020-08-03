994f5c695d3938c87a0b8103f781b69a0008359c
Mattheus Lima <61195848+mattheuslima@users.noreply.github.com>
2020-08-03 11:50:39 -0300

    Create teste 01.py

---
+++ teste 01.py
@@ -0,0 +1,15 @@
+print('====== DESAFIO 01 ====== ')
+nome=input('Qual é o seu nome?')
+print ('Olá ',nome , '! Prazer em te conhecer')
+print ('Por favor, informe sua data de nascimento:')
+dia=input ('Que dia você nasceu?')
+mes=input ('Que mês você nasceu? (por extenso)')
+ano=input ('Que ano você nasceu?')
+print (nome,', você nasceu em ',dia,' de', mes,' de',ano, ' . ')
+
+
+numum=input ('Escolha um número: ')
+numdois=input ('Escolha outro número: ')
+result= int(numum) + int(numdois)
+print (' Soma entre os números é ', result)
+
