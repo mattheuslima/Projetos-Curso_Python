94290b10431fe9b8f4c596e4f806654a5826eaf0
mattheuslima <mattheus.lima01@outlook.com>
2021-01-04 08:23:35 -0300

    update

---
+++ Exercícios/Ex.63.py
@@ -0,0 +1,15 @@
+
+print('-='*10)
+print('{:=^20}'.format('Desafio 63'))
+print('-='*10)
+
+n1=int(input('Quantos termos você quer mostrar? '))
+cont=0
+fibo=0
+while cont<n1:
+
+    print('{}->'.format(fibo),end="")
+    cont += 1
+    fibo += -1
+
+
