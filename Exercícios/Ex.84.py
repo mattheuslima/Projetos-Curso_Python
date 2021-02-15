58cbfb099824188d8581e38b4de93020fbe73254
mattheuslima <mattheus.lima01@outlook.com>
2021-02-15 09:46:07 -0300

    Update Ex.84.py

--- Exercícios/Ex.84.py
+++ Exercícios/Ex.84.py
@@ -6,25 +6,33 @@ C) Uma listagem com as pessoas mais leves.'''
 print('-='*10)
 print('{:=^20}'.format('Desafio 84'))
 print('-='*10)
+geral=list()
 alunos=list()
 dados=list()
 pesos=list()
 while True:
     dados.append(str(input('Digite o nome: ')))
     dados.append(float(input('Digite o peso: ')))
-    alunos.append(dados[:])
+    geral.append(dados[:])
     dados.clear()
     ans=str(input('Quer continuar? [S/N]')).strip().upper()[0]
-    if ans=='N':
+    if ans == 'N' or ans == '':
         break
 
-print(f'\nUm total de {len(alunos)} alunos foram cadastrados.\n\nA lista de alunos cadastrados é: ')
-for p in alunos:
+print(f'\nUm total de {len(geral)} alunos foram cadastrados.\n\nA lista de alunos cadastrados é: ')
+for p in geral:
     print(f'\n{p[0]}, com {p[1]} KG.')
-for p in alunos:
-    pesos.append(p[0])
+    alunos.append(p[0])
     pesos.append(p[1])
-print(f'{pesos}. Maior peso {max(pesos)}')
-print(pesos.index(max(pesos)))
-#print(f'\nO maior peso é {max(pesos)},referente a {alunos[alunos.index(max(pesos))-1]}')
+posmaispesada=pesos.index(max(pesos))
+posmaisleve=pesos.index(min(pesos))
+print(f'\nO maior peso da lista é {max(pesos)}, referente a : ',end='')
+mai,men=max(pesos),min(pesos)
+for posi,p in enumerate(pesos):
+    if p == mai :
+        print(f'{alunos[posi]}...',end='')
+print(f'\nO menor peso da lista é {min(pesos)}, referente a : ',end='')
+for posi,p in enumerate(pesos):
+    if p == men :
+        print(f'{alunos[posi]}...',end='')
 
